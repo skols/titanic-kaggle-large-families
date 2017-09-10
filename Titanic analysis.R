508ff73c98ff1f77813d719578241af794c0fb3d
Michael Skolnik <mikeskolnik75@gmail.com>
2017-09-09 20:10:21 -0400

    Second commit - Decision tree

--- Titanic analysis.R
+++ Titanic analysis.R
@@ -3,6 +3,7 @@ library(dplyr)
 library(ggplot2)
 library(caret)
 library(rpart)
+library(rpart.plot)
 
 # Load data and setup factors
 loaddata <- function(file) {
@@ -90,3 +91,98 @@ rpred <- predict(rmodel, newdata=test, type="class")
 accuracy <- modelaccuracy(test, rpred)
 bestaccuracy <- accuracy # initial base accuracy
 print(c("accuracy1", accuracy))
+
+# Add Pclass variable
+fol <- formula(Survived ~ Age + Sex + Pclass)
+rmodel <- rpart(fol, method="class", data=train)
+rpred <- predict(rmodel, newdata=test, type="class")
+accuracy <- modelaccuracy(test, rpred)
+accuracyLabel <- checkaccuracy(accuracy)
+print(c("accuracy2", accuracy, accuracyLabel))
+
+# Substitute Pclass with Fare variable
+fol <- formula(Survived ~ Age + Sex + Fare)
+rmodel <- rpart(fol, method="class", data=train)
+rpred <- predict(rmodel, newdata=test, type="class")
+accuracy <- modelaccuracy(test, rpred)
+accuracyLabel <- checkaccuracy(accuracy)
+print(c("accuracy3", accuracy, accuracyLabel))
+
+# Add back Pclass
+fol <- formula(Survived ~ Age + Sex + Pclass + Fare)
+rmodel <- rpart(fol, method="class", data=train)
+rpred <- predict(rmodel, newdata=test, type="class")
+accuracy <- modelaccuracy(test, rpred)
+accuracyLabel <- checkaccuracy(accuracy)
+print(c("accuracy4", accuracy, accuracyLabel))
+
+# Add SibSp + Parch
+fol <- formula(Survived ~ Age + Sex + Pclass + Fare + SibSp + Parch)
+rmodel <- rpart(fol, method="class", data=train)
+rpred <- predict(rmodel, newdata=test, type="class")
+print(rmodel)
+accuracy <- modelaccuracy(test, rpred)
+accuracyLabel <- checkaccuracy(accuracy)
+print(c("accuracy5", accuracy, accuracyLabel))
+
+# Test if Deck letter helps predict Survivability
+# Strip off cabin numbers
+# Extract the deck number
+# First letter: Deck (e.g. A31 -> A)
+
+# Make sure Deck in both sets has same levels
+# If Test has T but not Train different levels causes error in model
+train$Deck <- substr(train$Cabin,1,1)
+train$Deck[train$Deck==""] = NA
+test$Deck <- substr(test$Cabin,1,1)
+test$Deck[test$Deck==""] = NA
+
+train$Deck <- as.factor(train$Deck)
+test$Deck <- as.factor(test$Deck)
+
+# Make Deck have the same levels
+c <- union(levels(train$Deck), levels(test$Deck))
+levels(test$Deck) <- c
+levels(train$Deck) <- c
+
+# Test if Deck letter improves the prediction
+fol <- formula(Survived ~ Age + Sex + Pclass + Fare + SibSp + Parch + Deck)
+rmodel <- rpart(fol, method="class", data=train)
+rpred <- predict(rmodel, newdata=test, type="class")
+accuracy <- modelaccuracy(test, rpred)
+accuracyLabel <- checkaccuracy(accuracy)
+print(c("accuracy6", accuracy, accuracyLabel))
+
+# Check if FamilySize is a useful predictor of Survival
+fol <- formula(Survived ~ Age + Sex + Pclass + FamilySize)
+rmodel <- rpart(fol, method="class", data=train)
+rpred <- predict(rmodel, newdata=test, type="class")
+print(rmodel)
+accuracy <- modelaccuracy(test, rpred)
+accuracyLabel <- checkaccuracy(accuracy)
+print(c("accuracy7", accuracy, accuracyLabel)) # best so far
+
+p <- ggplot(aes(x=Pclass, y=factor(FamilySize), color=Survived), data=data) +
+  geom_jitter() +
+  facet_grid(Sex ~ .)
+p + ggtitle("Large Family Size >=5 more likely to not survive") +
+  theme_bw() +
+  geom_hline(yintercept = 5) +
+  ylab("Family Size")
+
+# If a person had 5 family members or more (including themselves) then it is more likely
+# the person didn’t survive especially in the third class which had entire families onboard.
+mosaicplot(table(FamilySize=data$FamilySize, Survived=data$Survived),
+           main="Passenger Survival by Family Size",
+           color=c("#fb8072", "#8dd3c7"), cex.axis = 1.2)
+# Mosaic plot above clearly shows the drop-off survival at family size of 5 or greater.
+
+# Make explicit factor levels for specific variables: Sex and Pclass
+titanic_test$Sex <- as.factor(titanic_test$Sex)
+titanic_test$Pclass <- as.factor(titanic_test$Pclass)
+
+# Now train on entire training set
+fol <- formula(Survived ~ Age + Sex + Pclass + FamilySize)
+model <- rpart(fol, method="class", data=data)
+rpart.plot(model, branch=0, branch.type=2, type=1, extra=102, shadow.col="pink", box.col="grey",
+           split.col="magenta", main="Decision tree for model")
